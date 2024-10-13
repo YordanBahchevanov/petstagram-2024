24e970c1293cd5d38753a6a71b4cf5f7fc25f6dd
DanDan93SB <yordanbah9376@gmail.com>
2024-10-13 19:07:16 +0300

    feat: Adding forms to the Photo and Pet models

--- petstagram/petstagram/pets/views.py
+++ petstagram/petstagram/pets/views.py
@@ -1,17 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from petstagram.pets.forms import PetAddForm, PetEditForm, PetDeleteForm
+from petstagram.pets.models import Pet
 
 
 def pet_add_page(request):
-    return render(request, 'pets/pet-add-page.html')
+    form = PetAddForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('profile-details', pk=1)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'pets/pet-add-page.html', context)
 
 
 def pet_delete_page(request, username: str, pet_slug: str):
-    return render(request, 'pets/pet-delete-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+    form = PetDeleteForm(instance=pet)
 
+    if request.method == "POST":
+        pet.delete()
+        return  redirect('profile-details', pk=1)
 
-def pet_details_page(request, username: str, pet_slug: str):
-    return render(request, 'pets/pet-details-page.html')
+    context = {
+        'form': form,
+        'pet': pet
+    }
+
+    return render(request, 'pets/pet-delete-page.html', context)
 
 
 def pet_edit_page(request, username: str, pet_slug: str):
-    return render(request, 'pets/pet-edit-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+    form = PetEditForm(request.POST or None, instance=pet)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('pet-details', username, pet_slug)
+
+    context = {
+        'form': form,
+        'pet': pet
+    }
+
+    return render(request, 'pets/pet-edit-page.html', context)
+
+
+def pet_details_page(request, username: str, pet_slug: str):
+    pet = Pet.objects.get(slug=pet_slug)
+    all_photos = pet.photo_set.all()
+
+    context = {
+        'pet': pet,
+        'all_photos': all_photos,
+    }
+
+    return render(request, 'pets/pet-details-page.html', context=context)
--- petstagram/petstagram/photos/views.py
+++ petstagram/petstagram/photos/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from petstagram.photos.forms import PhotoAddForm
+from petstagram.photos.models import Photo
 
 
 def photo_add_page(request):
-    return render(request, 'photos/photo-add-page.html')
+    form = PhotoAddForm(request.POST or None, request.FILES or None)
 
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('home')
 
-def photo_details_page(request, pk: int):
-    return render(request, 'photos/photo-details-page.html')
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'photos/photo-add-page.html', context)
 
 
 def photo_edit_page(request, pk: int):
     return render(request, 'photos/photo-edit-page.html')
+
+
+def photo_details_page(request, pk: int):
+    photo = Photo.objects.get(pk=pk)
+    likes = photo.like_set.all()
+    comments = photo.comment_set.all()
+
+    context = {
+        'photo': photo,
+        'likes': likes,
+        'comments': comments,
+    }
+
+    return render(request, 'photos/photo-details-page.html', context)
+
